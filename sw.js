90267430d34f06bf93cdb38f292b2bb30c66a461
AlleRock <m8online@gmail.com>
2026-02-17 21:58:44 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'aletamoney-v8';
+const CACHE_NAME = 'aletamoney-v8.2';
 const ASSETS = [
   './',
   './index.html',
@@ -7,27 +7,24 @@ const ASSETS = [
   'https://cdn.tailwindcss.com'
 ];
 
-// Installazione: salva i file nella cache
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
-    })
+    }).then(() => self.skipWaiting()) // Forza l'attivazione immediata
   );
 });
 
-// Attivazione: pulisce vecchie versioni della cache
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
       return Promise.all(
         keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
       );
-    })
+    }).then(() => self.clients.claim()) // Prende il controllo delle pagine subito
   );
 });
 
-// Fetch: serve i file dalla cache se sei offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
